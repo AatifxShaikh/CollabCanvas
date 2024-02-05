b6f7bd9cefb82787d77147bdcffdd48304054703
AatifxShaikh <96722552+AatifxShaikh@users.noreply.github.com>
2024-02-06 00:04:13 +0530

    Room ToolBar Added

--- app/boards/[boardId]/_components/toolbar.tsx
+++ app/boards/[boardId]/_components/toolbar.tsx
@@ -1,25 +1,50 @@
+import { Circle, MousePointer, MousePointer2, Pen, Square, StickyNote, Type } from "lucide-react"
+import { ToolButton } from "./tool-button"
 
 
 export const ToolBar = () => {
     return (
         <div className="absolute top-[50%] -translate-y-[50%] left-2 flex flex-col gap-y-4">
             <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-                <div>
-                    adding
-                </div>
+                <ToolButton
+                    label="Select"
+                    icon={MousePointer2}
+                    onClick={() => { }}
+                    isActive={false} />
+                <ToolButton
+                    label="Sticky"
+                    icon={StickyNote}
+                    onClick={() => { }}
+                    isActive={false}
+                />
+                <ToolButton
+                    label="Rectangle"
+                    icon={Square}
+                    onClick={() => { }}
+                    isActive={false}
+                />
+                <ToolButton
+                    label="Ellipse"
+                    icon={Circle}
+                    onClick={() => { }}
+                    isActive={false}
+                />
+                <ToolButton
+                    label="Pen"
+                    icon={Pen}
+                    onClick={() => { }}
+                    isActive={false}
+                />
+                <ToolButton
+                    label="Text"
+                    icon={Type}
+                    onClick={() => { }}
+                    isActive={false}
+                />
 
-                <div>
-                    data
-                </div>
-
-                <div>
-                    for
-                </div>
-                <div>
-                    testing
-                </div>
 
             </div>
+
             <div className="bg-white rounded-md flex p-1.5 flex-col items-center  shadow-md">
                 <div>
                     Undo
